68bf15c7b2dd7563942ad665dc53559e9c1edc9e
somes <somes@SOMESH>
2024-06-30 19:35:33 +0530

    Add project files.

---
+++ ListToCsv.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using System.Text;
+
+
+namespace ListToCsv
+{
+    public static class ListExtensions
+    {
+        public static string ToCsv<T>(this List<T> list, string delimiter = ",", bool includeHeaders = true )
+        {
+            if(list == null || list.Count == 0)
+            {
+                return string.Empty;
+            }
+            
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            StringBuilder sb = new StringBuilder();
+
+            if(includeHeaders)
+            {
+                sb.AppendLine(string.Join(delimiter, properties.Select(p => p.Name)));
+            }
+
+            foreach (T item in list)
+            {
+                var value = properties.Select(p => EscapeForCsv(p.GetValue(item)?.ToString() ?? string.Empty));
+                sb.AppendLine(string.Join(delimiter, value));
+            }
+
+            return sb.ToString();
+        }
+
+        private static string EscapeForCsv(string value)
+        {
+            if(string.IsNullOrEmpty(value))
+            {
+                return string.Empty;
+            }
+
+            if(value.Contains(",") || value.Contains("\"") || value.Contains("\n") || value.Contains("\r"))
+            {
+                return "\"" + value.Replace("\"", "\"\"") + "\"";
+            }
+
+            return value;
+        }
+    }
+}
